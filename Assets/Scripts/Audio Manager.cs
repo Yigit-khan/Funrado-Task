66513e60801bdb06443728a2fd197e5a977cd9a7
Yigit-khan <yigitkangirisit02@hotmail.com>
2024-05-31 18:09:52 +0300

    update

---
+++ Assets/Scripts/Audio Manager.cs
@@ -0,0 +1,30 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+        
+    }
+    public void Start()
+    {
+        Play("Theme");
+    }
+    // Update is called once per frame
+    public void Play (string name)
+    {
+        Sound s = Array.Find(sounds, Sound => Sound.name == name);
+        s.source.Play();
+    }
+}
